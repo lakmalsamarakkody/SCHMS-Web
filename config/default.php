9c36237845465e082f4789c8f171aa8afc938648
lakmalsamarakkody <lakmalsamarakkody@gmail.com>
2019-05-31 00:15:55 +0530

    config/default base URL changed

--- config/default.php
+++ config/default.php
@@ -5,7 +5,7 @@ $_['site_title']			=	'Skeleton';
 
 // APP CONFIG
 $_['app_theme']				=	'default';
-$_['base_url']				=	'http://skeleton.int';
+$_['base_url']				=	'http://bit-project.int';
 
 // TWIG
 $_['twig_debug']			=	true;
