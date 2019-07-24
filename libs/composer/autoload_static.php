09d3acf78b6cc75432fd3e1c923050995820c08c
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-07-24 22:09:00 +0530

    composer and gump added

--- libs/composer/autoload_static.php
+++ libs/composer/autoload_static.php
@@ -10,6 +10,7 @@ class ComposerStaticInit30d1bc2514a9fa5790ef9a9668be9260
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '72579e7bd17821bb1321b87411366eae' => __DIR__ . '/..' . '/illuminate/support/helpers.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        'da253f61703e9c22a5a34f228526f05a' => __DIR__ . '/..' . '/wixel/gump/gump.class.php',
     );
 
     public static $prefixLengthsPsr4 = array (
