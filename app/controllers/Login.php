e5c27315d29ca16dbf7046cd1ffc827d754995ab
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-07-25 00:36:15 +0530

    login controller created

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -15,6 +15,56 @@ class Login extends Controller {
 		//Login
 		if (isset($_POST['is_submit'])):
 
+			$this->load->model('User');
+
+			$validate = GUMP::is_valid($_POST,['username' => 'required']);
+
+			if($validate == true):
+
+				$User = $this->model_user::where('username',$_POST['username']) ->first();
+
+				if($User != null):
+
+					if(password_verify ($_POST['password'],$User->password) == true):
+
+						$data['msg'] = "Logged in Successfully";
+
+					else:
+
+						$data['msg'] = "Incorrect Password";
+
+					endif;
+
+				else:
+
+					$User = $this->model_user::where('emp_id',$_POST['username']) -> where('username',"") -> where('password',"") ->first();
+
+					if($User != null):
+
+						if($_POST['password'] == $User->nic):
+
+							$data['msg'] = "New User detected";
+
+						else:
+
+							$data['msg'] = "Incorrect NIC";
+
+						endif;
+					
+					else:
+
+						$data['msg'] = "No User found";
+
+					endif;
+
+				endif;
+			
+			else:
+
+				$data['msg'] = "Enter your Username";
+
+			endif;
+
 		endif;
 
 		// RENDER VIEW
--- config/default.php
+++ config/default.php
@@ -11,13 +11,13 @@ $_['base_url']				=	'http://bit-project.int';
 $_['twig_debug']			=	true;
 
 // DATABASE
-// $_['db_driver']				=	'mysql';
-// $_['db_charset']				=	'utf8';
-// $_['db_collation']			=	'utf8_unicode_ci';
-// $_['db_host']				=	'localhost';
-// $_['db_name']				=	'';
-// $_['db_user']				=	'';
-// $_['db_pass']				=	'';
-// $_['db_prefix']				=	'';
+$_['db_driver']				=	'mysql';
+$_['db_charset']				='utf8';
+$_['db_collation']			=	'utf8_unicode_ci';
+$_['db_host']				=	'localhost';
+$_['db_name']				=	'schms';
+$_['db_user']				=	'schms';
+$_['db_pass']				=	'admin';
+$_['db_prefix']				=	'';
 
 ?>
