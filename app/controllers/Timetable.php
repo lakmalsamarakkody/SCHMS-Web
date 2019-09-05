9642dbfd6f602f9262af489da925fcd319abf430
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-09-06 01:44:26 +0530

    exam schdule basic over

---
+++ app/controllers/Timetable.php
@@ -0,0 +1,41 @@
+<?php
+
+class Timetable extends Controller {
+    public function index() {
+    
+        // SITE DETAILS
+		$data['app']['url']			= $this->config->get('base_url');
+		$data['app']['title']		= $this->config->get('site_title');
+		$data['app']['theme']		= $this->config->get('app_theme');
+
+		// HEADER / FOOTER
+		$data['template']['header']		= $this->load->controller('common/header', $data);
+        $data['template']['footer']		= $this->load->controller('common/footer', $data);
+        $data['template']['sidenav']	= $this->load->controller('common/sidenav', $data);
+        $data['template']['topmenu']	= $this->load->controller('common/topmenu', $data);
+
+		// RENDER VIEW
+        $this->load->view('timetable/index', $data);
+        
+    }
+    
+    public function create() {
+    
+        // SITE DETAILS
+		$data['app']['url']			= $this->config->get('base_url');
+		$data['app']['title']		= $this->config->get('site_title');
+		$data['app']['theme']		= $this->config->get('app_theme');
+
+		// HEADER / FOOTER
+		$data['template']['header']		= $this->load->controller('common/header', $data);
+        $data['template']['footer']		= $this->load->controller('common/footer', $data);
+        $data['template']['sidenav']	= $this->load->controller('common/sidenav', $data);
+        $data['template']['topmenu']	= $this->load->controller('common/topmenu', $data);
+
+		// RENDER VIEW
+        $this->load->view('timetable/create', $data);
+        
+    } 
+    
+}
+?>
