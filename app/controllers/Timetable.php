623dcf9e59c55df0486162f4acafc39308c3c951
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-09-06 15:20:24 +0530

    sidenav modified

--- app/controllers/Timetable.php
+++ app/controllers/Timetable.php
@@ -19,6 +19,24 @@ class Timetable extends Controller {
         
     }
     
+    public function search() {
+    
+        // SITE DETAILS
+		$data['app']['url']			= $this->config->get('base_url');
+		$data['app']['title']		= $this->config->get('site_title');
+		$data['app']['theme']		= $this->config->get('app_theme');
+
+		// HEADER / FOOTER
+		$data['template']['header']		= $this->load->controller('common/header', $data);
+        $data['template']['footer']		= $this->load->controller('common/footer', $data);
+        $data['template']['sidenav']	= $this->load->controller('common/sidenav', $data);
+        $data['template']['topmenu']	= $this->load->controller('common/topmenu', $data);
+
+		// RENDER VIEW
+        $this->load->view('timetable/search', $data);
+        
+    } 
+
     public function create() {
     
         // SITE DETAILS
