2ceb0adebacf2ac8a157c005071e17f959ecc38f
Shyamin Ayesh <me@shyamin.com>
2019-09-16 03:51:49 +0530

    implement user insert

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -61,7 +61,56 @@ class User extends Controller {
 		// RENDER VIEW
         $this->load->view('user/add', $data);
         
-    } 
+    }
+
+
+    public function ajax_add() {
+
+        /**
+         * This method will receive ajax request from
+         * the front end with the following payload
+         *    - role_id
+         *    - username
+         *    - pass
+         *    - pass_confirm
+         *    - email
+         * 
+         * We need to validate the data and then perform
+         * the following tasks.
+         *    - validate
+         *    - CRUD
+         *    - response ( JSON )
+         */
+
+        // SET JSON HEADER
+        header('Content-Type: application/json');
+
+        if ( GUMP::is_valid($this->request->post, array('role_id' => 'required|integer')) !== true ):
+            echo json_encode( array( "error" => "We didn't got valid role id ?" ), JSON_PRETTY_PRINT );
+            exit();
+        endif;
+
+        if ( GUMP::is_valid($this->request->post, array('username' => 'required|alpha_dash')) !== true ):
+            echo json_encode( array( "error" => "We didn't got valid username ?" ), JSON_PRETTY_PRINT );
+            exit();
+        endif;
+
+        // MODEL
+        $this->load->model('user');
+
+        $this->model_user->role_id = $this->request->post['role_id'];
+        $this->model_user->username = $this->request->post['username'];
+        $this->model_user->password = password_hash($this->request->post['pass'], PASSWORD_DEFAULT);
+        $this->model_user->email = $this->request->post['email'];
+        
+        
+        if ( $this->model_user->save() ):
+            echo json_encode( array( "status" => "success" ), JSON_PRETTY_PRINT );
+        else:
+            echo json_encode( array( "status" => "failed" ), JSON_PRETTY_PRINT );
+        endif;
+
+    }
     
 }
 ?>
--- app/models/User.php
+++ app/models/User.php
@@ -5,8 +5,13 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class User_Model extends EloquentModel {
 
+	const CREATED_AT = 'created_on';
+	const UPDATED_AT = 'updated_on';
+
 	// PARENT PROPERTIES
 	protected $table = 'user';
+	protected $fillable = [ 'id', 'stu_staff_parent_id', 'nic', 'username', 'password', 'email', 'role_id', 'status' ];
+
 }
 
 ?>
