f406d047a2ece8e8c93ac2fcabe87df611f665b0
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-10-05 00:02:23 +0530

    sport/add UI under development

--- app/controllers/Sport.php
+++ app/controllers/Sport.php
@@ -50,9 +50,41 @@ class Sport extends Controller {
         $data['template']['sidenav']	= $this->load->controller('common/sidenav', $data);
         $data['template']['topmenu']	= $this->load->controller('common/topmenu', $data);
 
+        // MODEL
+        $this->load->model('sport');
+        $this->load->model('district');
+
+        // SPORT
+		foreach( $this->model_sport->select('id', 'name')->orderBy('id')->get() as $key => $element ):
+			$data['sports'][$key]['id'] = $element->id;
+			$data['sports'][$key]['name']= $element->name;
+        endforeach;
+        
+        //DISTRICT
+        foreach( $this->model_district->select('id', 'province_id', 'name')->orderBy('name')->get() as $key => $element ):
+            $data['student_district'][$key]['id'] = $element->id;
+            $data['student_district'][$key]['province']['id'] = $element->province_id;
+            $data['student_district'][$key]['name'] = $element->name;
+        endforeach;
+
 		// RENDER VIEW
         $this->load->view('sport/add', $data);
         
+    }
+
+    public function ajax_retrive_province_by_district($id) {
+
+        // SET JSON HEADER
+        header('Content-Type: application/json');
+
+        // MODEL
+        $this->load->model('district');
+        $this->load->model('province');
+
+        $province_id = $this->model_district->select('province_id')->where('id', '=', $id)->first()->province_id;
+
+        echo json_encode(array( "status" => "success", "data" => $this->model_province->select('id', 'name')->where('id', '=', $province_id)->first() ));
+
     }
 }
 ?>
