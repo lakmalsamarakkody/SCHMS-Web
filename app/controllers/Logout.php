01021f696d4eade1e6a39d5f3a0c2e955c8fcb90
Shyamin Ayesh <me@shyamin.com>
2019-11-26 19:26:00 +0530

    beautify

--- app/controllers/Logout.php
+++ app/controllers/Logout.php
@@ -2,12 +2,8 @@
 
 class Logout extends Controller {
     public function index() {
-
-        //Logout
         session_destroy();
-        // header( 'Location: ' . "http://davidsilvacollege.int" );
-        header( 'Location: ' . $this->config->get('base_url') . "/login" );
-
+        header('Location: ' . $this->config->get('base_url') . "/login");
     }
 }
 
