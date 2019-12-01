7e43e3a7cb957a38734eb07279234b372a218c72
Shyamin Ayesh <me@shyamin.com>
2019-12-02 04:31:26 +0530

    bugfix : sport chart grouping

--- app/controllers/Sport.php
+++ app/controllers/Sport.php
@@ -74,7 +74,7 @@ class Sport extends Controller {
         $data['sport']['student']['latest']['achievement'] = $this->model_student_sportachievement->select('*')->orderBy('id', 'DESC')->first(1);
 
         // APEX CHARTS
-        foreach ( $this->model_student_sport->select('id', 'student_id', 'sport_id')->distinct()->get() as $key => $element ):
+        foreach ( $this->model_student_sport->select('id', 'student_id', 'sport_id')->groupBy('sport_id')->get() as $key => $element ):
             $data['sports'][$key]['name'] = $this->model_sport->select('name')->where('id', '=', $element->sport_id)->first()->name;
             $data['sports'][$key]['count'] = $this->model_student_sport->select('student_id')->where('sport_id', '=', $element->sport_id)->count();
         endforeach;
