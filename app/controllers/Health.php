bab3933022dced3e0e16d3aa223655dfd2d687c3
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-12-10 19:42:13 +0530

    health search finished

--- app/controllers/Health.php
+++ app/controllers/Health.php
@@ -187,6 +187,16 @@ class Health extends Controller {
 			$data['sports'][$key]['name']= $element->name;
         endforeach;
 
+        // BLOOD GROUPS
+        $data['blood']['types'][2]['name'] = 'A+';
+        $data['blood']['types'][1]['name'] = 'A-';
+        $data['blood']['types'][4]['name'] = 'B+';
+        $data['blood']['types'][3]['name'] = 'B-';
+        $data['blood']['types'][6]['name'] = 'AB+';
+        $data['blood']['types'][5]['name'] = 'AB-';
+        $data['blood']['types'][8]['name'] = 'O+';
+        $data['blood']['types'][7]['name'] = 'O-';
+
         // SEARCH STUDENT
         if( isset( $this->request->post['isSubmitedStudent']) ):
             
@@ -196,6 +206,12 @@ class Health extends Controller {
             $data['form']['field']['student_grade'] = ( isset($this->request->post['student_grade']) AND !empty($this->request->post['student_grade']) ) ? $this->request->post['student_grade'] : "";
             $data['form']['field']['student_gender'] = ( isset($this->request->post['student_gender']) AND !empty($this->request->post['student_gender']) ) ? $this->request->post['student_gender'] : "";
             $data['form']['field']['student_sport'] = ( isset($this->request->post['student_sport']) AND !empty($this->request->post['student_sport']) ) ? $this->request->post['student_sport'] : "";
+            $data['form']['field']['student_min_height'] = ( isset($this->request->post['student_min_height']) AND !empty($this->request->post['student_min_height']) ) ? $this->request->post['student_min_height'] : "";
+            $data['form']['field']['student_max_height'] = ( isset($this->request->post['student_max_height']) AND !empty($this->request->post['student_max_height']) ) ? $this->request->post['student_max_height'] : "";
+            $data['form']['field']['student_min_weight'] = ( isset($this->request->post['student_min_weight']) AND !empty($this->request->post['student_min_weight']) ) ? $this->request->post['student_min_weight'] : "";
+            $data['form']['field']['student_max_weight'] = ( isset($this->request->post['student_max_weight']) AND !empty($this->request->post['student_max_weight']) ) ? $this->request->post['student_max_weight'] : "";
+            $data['form']['field']['student_bmi'] = ( isset($this->request->post['student_bmi']) AND !empty($this->request->post['student_bmi']) ) ? $this->request->post['student_bmi'] : "";
+            $data['form']['field']['student_blood_group'] = ( isset($this->request->post['student_blood_group']) AND !empty($this->request->post['student_blood_group']) ) ? $this->request->post['student_blood_group'] : "";
 
             // Eloquent OBJECT
             $student = $this->model_student->select('id', 'admission_no', 'admission_date', 'class_id', 'full_name', 'initials', 'surname', 'dob', 'gender', 'email', 'phone_mobile', 'address', 'city', 'district_id', 'religion_id');
@@ -248,16 +264,92 @@ class Health extends Controller {
                 });
             endif;
 
-            // HEIGHT
-            // if ( isset($this->request->post['height_min']) AND !empty($this->request->post['height_min']) ):
-            //     $student->where(function($query) {
-            //         $query->where('height', '>', $this->request->post['height_min']);
-            //     });
-            // endif;
+            // FILTER (HEIGHT_MIN)
+            if ( isset($this->request->post['student_min_height']) AND !empty($this->request->post['student_min_height']) ):
+                $student_min_height = $this->model_student_health->select('student_id')->where('height', '>=', $this->request->post['student_min_height'])->get();
+                $health_height_min_ids = array();
+                foreach( $student_min_height as $el ):
+                    array_push($health_height_min_ids, $el->student_id);
+                endforeach;
 
+                $student->where(function($query) use ($health_height_min_ids) {
+                    $query->whereIn('id', $health_height_min_ids);
+                });
+            endif;
+            // FILTER (HEIGHT_MAX)
+            if ( isset($this->request->post['student_max_height']) AND !empty($this->request->post['student_max_height']) ):
+                $student_max_height = $this->model_student_health->select('student_id')->where('height', '<=', $this->request->post['student_max_height'])->get();
+                $health_height_max_ids = array();
+                foreach( $student_max_height as $el ):
+                    array_push($health_height_max_ids, $el->student_id);
+                endforeach;
+
+                $student->where(function($query) use ($health_height_max_ids) {
+                    $query->whereIn('id', $health_height_max_ids);
+                });
+            endif;
+
+            // FILTER (WEIGHT_MIN)
+            if ( isset($this->request->post['student_min_weight']) AND !empty($this->request->post['student_min_weight']) ):
+                $student_min_weight = $this->model_student_health->select('student_id')->where('weight', '>=', $this->request->post['student_min_weight'])->get();
+                $health_weight_min_ids = array();
+                foreach( $student_min_weight as $el ):
+                    array_push($health_weight_min_ids, $el->student_id);
+                endforeach;
+
+                $student->where(function($query) use ($health_weight_min_ids) {
+                    $query->whereIn('id', $health_weight_min_ids);
+                });
+            endif;
+            // FILTER (WEIGHT_MAX)
+            if ( isset($this->request->post['student_max_weight']) AND !empty($this->request->post['student_max_weight']) ):
+                $student_max_weight = $this->model_student_health->select('student_id')->where('weight', '<=', $this->request->post['student_max_weight'])->get();
+                $health_weight_max_ids = array();
+                foreach( $student_max_weight as $el ):
+                    array_push($health_weight_max_ids, $el->student_id);
+                endforeach;
+
+                $student->where(function($query) use ($health_weight_max_ids) {
+                    $query->whereIn('id', $health_weight_max_ids);
+                });
+            endif;
+
+            // FILTER (BMI)
+            if ( isset($this->request->post['student_bmi']) AND !empty($this->request->post['student_bmi']) ):
+                if ( $this->request->post['student_bmi'] == "below-average" ):
+                    $student_bmi = $this->model_student_health->select('student_id')->where('bmi', '<', 18.5)->get();
+                elseif ( $this->request->post['student_bmi'] == "in-average" ):
+                    $student_bmi = $this->model_student_health->select('student_id')->where('bmi', '>=', 18.5)->where('bmi', '<=', 25)->get();
+                else:
+                    $student_bmi = $this->model_student_health->select('student_id')->where('bmi', '>', 25)->get();
+                endif;
+                
+                $student_bmi_ids = array();
+                foreach( $student_bmi as $el ):
+                    array_push($student_bmi_ids, $el->student_id);
+                endforeach;
+
+                $student->where(function($query) use ($student_bmi_ids) {
+                    $query->whereIn('id', $student_bmi_ids);
+                });
+            endif;
+
+            // FILTER (BMI)
+            if ( isset($this->request->post['student_blood_group']) AND !empty($this->request->post['student_blood_group']) ):
+                $student_blood_group = $this->model_student_health->select('student_id')->where('blood_group', '=', $this->request->post['student_blood_group'])->get();
+                $student_blood_group_ids = array();
+                foreach( $student_blood_group as $el ):
+                    array_push($student_blood_group_ids, $el->student_id);
+                endforeach;
+
+                $student->where(function($query) use ($student_blood_group_ids) {
+                    $query->whereIn('id', $student_blood_group_ids);
+                });
+            endif;
 
             // APPEND DATA TO ARRAY
             foreach( $student->get() as $key => $value ):
+                
                 $data['students'][$key]['id'] = $value->id;
                 $data['students'][$key]['admission_no'] = $value->admission_no;
                 $data['students'][$key]['name'] = $value->initials." ".$value->surname;
@@ -265,6 +357,21 @@ class Health extends Controller {
                 $data['students'][$key]['dob'] = $value->dob;
                 $data['students'][$key]['class'] = $value->class_id;
                 $data['students'][$key]['city'] = $value->city;
+
+                $health_data = $this->model_student_health->where('student_id', '=', $value->id)->first();
+                if ( $health_data !== NULL):
+                    $data['students'][$key]['health_id'] = $health_data->id;
+                    $data['students'][$key]['height'] = $health_data->height;
+                    $data['students'][$key]['weight'] = $health_data->weight;
+                    $data['students'][$key]['bmi'] = $health_data->bmi;
+                    $data['students'][$key]['hr'] = $health_data->heart_rate;
+                    $data['students'][$key]['bp'] = $health_data->blood_pressure;
+                    $data['students'][$key]['bg'] = $health_data->blood_group;
+                    $data['students'][$key]['speciality'] = $health_data->speciality;
+                    $data['students'][$key]['vaccination'] = $health_data->vaccination;
+                    $data['students'][$key]['date'] = $health_data->date;
+                endif;
+
             endforeach;
 
         endif;
@@ -415,7 +522,41 @@ class Health extends Controller {
 
 		// RENDER VIEW
         $this->load->view('health/add', $data);
-        
     }
+
+    public function ajax_removehealth() {
+
+		//CHECK LOGIN STATUS
+		if( !isset($_SESSION['user']) OR $_SESSION['user']['is_login'] != true ):
+			header( 'Location:' . $this->config->get('base_url') . '/logout' );
+			exit();
+		endif;
+
+		// SET JSON HEADER
+		header('Content-Type: application/json');
+
+		// MODEL
+		$this->load->model('student/health');
+		
+        if ( isset($this->request->post['health_id']) AND !empty($this->request->post['health_id']) ):
+            $is_valid_health_id = $this->model_student_health->select('id')->where('id', '=', $this->request->post['health_id']);
+
+			if ( $is_valid_health_id->first() !== NULL ):
+				if ( $this->model_student_health->find($this->request->post['health_id'])->delete() ):
+					echo json_encode( array( "status" => "success" ), JSON_PRETTY_PRINT );
+					exit();
+				else:
+					echo json_encode( array( "status" => "error", "message" => "Cannot delete this health record. Please contact system administrator" ), JSON_PRETTY_PRINT );
+                    exit();
+				endif;
+			else:
+				echo json_encode( array( "status" => "error", "message" => "No record found" ), JSON_PRETTY_PRINT );
+				exit();
+			endif;
+		else:
+			echo json_encode( array( "status" => "error", "message" => "Please select a valid health record" ), JSON_PRETTY_PRINT );
+			exit();
+		endif;
+	}
 }
 ?>
