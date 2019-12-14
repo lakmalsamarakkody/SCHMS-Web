edc95e6000596a36e77a932278b66bf2f2096002
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-12-14 14:12:45 +0530

    fixed issue with student admission number increment process

--- app/controllers/Student.php
+++ app/controllers/Student.php
@@ -469,11 +469,11 @@ class Student extends Controller {
 
             // ADMISSION NUMBER IS EMPTY : INCREMENT BY ONE TO THE LAST ADMISSION NUMBER
             else:
-                $this->request->post['admission_number'] = $this->model_student->select('admission_no')->orderBy('admission_no', 'DESC')->take(1)->first()->admission_no;
-                if ( $this->request->post['admission_number'] == NULL ):
+                $last_admission = $this->model_student->select('admission_no')->orderBy('admission_no', 'DESC')->take(1)->first();
+                if ( $last_admission == NULL ):
                     $this->request->post['admission_number'] = 1;
                 else:
-                    $this->request->post['admission_number']++;
+                    $this->request->post['admission_number'] = $last_admission->admission_no +1;
                 endif;                
             endif;
 
