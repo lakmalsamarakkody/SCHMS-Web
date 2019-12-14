ec4b37e72fdb0d6ad0f5be4e4e1223a82d8f3fbb
Srivin Prabhash <me@srivin.pw>
2019-12-14 07:38:40 +0530

    messages inbox partially designed

---
+++ app/controllers/Messages.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Capsule\Manager as DB;
+
+class Messages extends Controller {
+    
+    public function index(){
+
+        //CHECK LOGIN STATUS
+        if( !isset($_SESSION['user']) OR $_SESSION['user']['is_login'] != true ):
+            header( 'Location:' . $this->config->get('base_url') . '/portal/login' );
+            exit();
+        endif;
+
+		// SITE DETAILS
+		$data['app']['url']			= $this->config->get('base_url');
+		$data['app']['title']		= $this->config->get('site_title');
+		$data['app']['theme']		= $this->config->get('app_theme');
+
+		// HEADER / FOOTER
+		$data['template']['header']		= $this->load->controller('common/header', $data);
+		$data['template']['footer']		= $this->load->controller('common/footer', $data);
+		$data['template']['topmenu']	= $this->load->controller('common/topmenu', $data);
+
+
+        // LOAD CONVERSATIONS
+        $this->load->view('messages/index', $data);
+
+    }
+
+}
+?>
