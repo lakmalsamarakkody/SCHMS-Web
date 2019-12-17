033bbd29e7abda8945236af321fe701ea2bdfb92
Srivin Prabhash <me@srivin.pw>
2019-12-18 04:07:56 +0530

    implemented theme toggle

--- app/controllers/common/Topmenu.php
+++ app/controllers/common/Topmenu.php
@@ -8,6 +8,9 @@ class CommonTopmenu extends Controller {
 		$data['date_now'] = Carbon::now()->isoFormat('dddd, DD MMMM - YYYY');
 		$data['time_now'] = Carbon::now()->isoFormat( 'h:mm:ss A' );
 
+		$this->load->model('user');
+		$data['user']['theme'] = $this->model_user->findOrFail($_SESSION['user']['id'])->theme;
+
 		return $this->load_view('common/topmenu', $data);
 	}
 }
