dadb5499ec29373a48865dc83f1638b81122d342
Shyamin Ayesh <me@shyamin.com>
2019-12-18 23:39:17 +0530

    fix some bug in login screen

--- app/controllers/common/Header.php
+++ app/controllers/common/Header.php
@@ -4,7 +4,11 @@ class CommonHeader extends Controller {
 	public function index($data) {
 
 		$this->load->model('user');
-		$data['user']['theme'] = $this->model_user->findOrFail($_SESSION['user']['id'])->theme;
+		if ( isset($_SESSION['user']['id']) ):
+			$data['user']['theme'] = $this->model_user->findOrFail($_SESSION['user']['id'])->theme;
+		else:
+			$data['user']['theme'] = 'Default';
+		endif;
 
 		return $this->load_view('common/header', $data);
 	}
