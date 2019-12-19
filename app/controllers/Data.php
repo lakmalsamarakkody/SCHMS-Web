3026ad1d1d3f775e239c0f2c0a141fc92d7ff469
Lakmal Samarakkody <lakmalsamarakkody@gmail.com>
2019-12-19 07:09:42 +0530

    messages inbox completed

--- app/controllers/Data.php
+++ app/controllers/Data.php
@@ -35,11 +35,81 @@ class Data extends Controller {
                 // CHECK USER
                 $student = $this->model_student->select('id')->where('id', '=', $id)->first();
                 if ( $student !== null ):
-                    $image = imagecreatefromjpeg(ABS_PATH.'/data/uploads/propics/students/'.$id.'.jpg');
+
+                    // CHECK PROFILE PIC EXISTS
+                    $image = null;
+                    if ( file_exists( ABS_PATH.'/data/uploads/propics/student/'.$id.'.jpg' ) ):
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/uploads/propics/student/'.$id.'.jpg');
+                    else:
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/img/default_user.jpg');
+                    endif;
+
                     imagejpeg($image);
-                    $image->destroy();
+                    // $image->destroy();
                 endif;
 
+            elseif( $action == 'staff' ):
+
+                // MODELS
+                $this->load->model('staff');
+
+                // CHECK USER
+                $staff = $this->model_staff->select('id')->where('id', '=', $id)->first();
+                if ( $staff !== null ):
+
+                    // CHECK PROFILE PIC EXISTS
+                    $image = null;
+                    if ( file_exists( ABS_PATH.'/data/uploads/propics/staff/'.$id.'.jpg' ) ):
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/uploads/propics/staff/'.$id.'.jpg');
+                    else:
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/img/default_user.jpg');
+                    endif;
+
+                    imagejpeg($image);
+                    // $image->destroy();
+                endif;
+
+            elseif( $action == 'parent' ):
+
+                // MODELS
+                $this->load->model('parent');
+
+                // CHECK USER
+                $parent = $this->model_parent->select('id')->where('id', '=', $id)->first();
+                if ( $parent !== null ):
+
+                    // CHECK PROFILE PIC EXISTS
+                    $image = null;
+                    if ( file_exists( ABS_PATH.'/data/uploads/propics/parent/'.$id.'.jpg' ) ):
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/uploads/propics/parent/'.$id.'.jpg');
+                    else:
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/img/default_user.jpg');
+                    endif;
+
+                    imagejpeg($image);
+                    // $image->destroy();
+                endif;
+
+            elseif( $action == 'coach' ):
+
+                // MODELS
+                $this->load->model('coach');
+
+                // CHECK USER
+                $coach = $this->model_coach->select('id')->where('id', '=', $id)->first();
+                if ( $coach !== null ):
+
+                    // CHECK PROFILE PIC EXISTS
+                    $image = null;
+                    if ( file_exists( ABS_PATH.'/data/uploads/propics/coach/'.$id.'.jpg' ) ):
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/uploads/propics/coach/'.$id.'.jpg');
+                    else:
+                        $image = imagecreatefromjpeg(ABS_PATH.'/data/img/default_user.jpg');
+                    endif;
+
+                    imagejpeg($image);
+                    // $image->destroy();
+                endif;
 
             endif;
 
--- app/controllers/Messages.php
+++ app/controllers/Messages.php
@@ -27,11 +27,15 @@ class Messages extends Controller {
         // MODELS
         $this->load->model('user');
         $this->load->model('message');
+        $this->load->model('staff');
+        $this->load->model('student');
+        $this->load->model('parent');
+        $this->load->model('coach');
 
         if ( $id !== null ){
 
-            // single message
-            $messages = $this->model_message->select('id', 'sender_id', 'receiver_id', 'body');
+            // single conversation
+            $messages = $this->model_message->select('id', 'sender_id', 'receiver_id', 'body', 'created_on');
             $messages->where(function($query) {
                 $query->orWhere(function ($query) {
                     $query->where('receiver_id', $_SESSION['user']['id']);
@@ -48,7 +52,7 @@ class Messages extends Controller {
                     $query->where('sender_id', $id);
                 });
             });
-            $messages->orderBy('created_on', 'asc')->take(20);
+            $messages->orderBy('created_on', 'asc')->take(50);
 
             foreach ( $messages->get() as $key => $el ):
                 $data['messages'][$key]['id']           = $el->id;
@@ -56,10 +60,27 @@ class Messages extends Controller {
                 $data['messages'][$key]['sender_id']    = $el->sender_id;
                 $data['messages'][$key]['receiver_id']  = $el->receiver_id;
                 $data['messages'][$key]['body']         = $el->body;
+                $data['messages'][$key]['time']         = $el->created_on;
             endforeach;
 
-            $this->load->view('messages/single', $data);
+            // CHAT PERSON DATA
+            $participant_data = $this->model_user->select('ref_id', 'user_type')->where('id', '=',$id)->first();
+            $data['participant']['user_id'] = $id;
+            $data['participant']['id'] = $participant_data->ref_id;
+            $data['participant']['type'] = $this->model_user->select('user_type')->where('id', '=', $id)->first()->user_type;
+            $data['participant']['image']['path'] = $participant_data->user_type."/".$participant_data->ref_id;
 
+            $model_name = "model_".$data['participant']['type'];
+            $participant_name = $this->$model_name->select('initials', 'surname')->where('id', '=', $participant_data->ref_id)->first();
+            $data['participant']['name'] = $participant_name->initials." ".$participant_name->surname;
+
+            // YOUR DATA
+            $receiver_data = $this->model_user->select('ref_id', 'user_type')->where('id', '=',$_SESSION['user']['id'])->first();
+            $data['your']['id'] = $receiver_data->ref_id;
+            $data['your']['type'] = $receiver_data->user_type;
+            $data['your']['image']['path'] = $receiver_data->user_type."/".$receiver_data->ref_id;
+
+            $this->load->view('messages/single', $data);
         } else {
             
             /**
@@ -72,11 +93,13 @@ class Messages extends Controller {
 
                 $user = $this->model_user->find($element->sender_id);
 
+                // QUERY ( LAST MESSAGE )
                 // $messages = $this->model_message->select('id', 'body')->where('sender_id', $_SESSION['user']['id'])->where('receiver_id', $element->sender_id)->orderBy('created_on', 'desc')->first();
                 // if ( $messages === null ):
                 // $messages = $this->model_message->select('id', 'body')->where('receiver_id', $_SESSION['user']['id'])->where('sender_id', $element->sender_id)->orderBy('created_on', 'desc')->first();
                 // endif;
 
+                // QUERY ( LAST MESSAGE )
                 $messages = $this->model_message->select('id', 'body');
                 $messages->where(function($query) {
                     $query->orWhere(function ($query) {
@@ -100,17 +123,84 @@ class Messages extends Controller {
                 // RESOLVE SENDER
                 $data['converstations'][$key]['user']['username']           = $user->username;
                 $data['converstations'][$key]['user']['id']                 = $element->sender_id;
-                $data['converstations'][$key]['user']['type']               = $user->user_type;
                 $data['converstations'][$key]['user']['ref_id']             = $user->ref_id;
+                $data['converstations'][$key]['user']['type']               = $user->user_type;
                 $data['converstations'][$key]['user']['message']['body']    = $messages->body;
+
+                // CONVERSATION PERSON DATA
+                $data['converstations'][$key]['image']['path'] = $user->user_type."/".$user->ref_id;
+                $model_name = "model_".$user->user_type;
+                $participant_name = $this->$model_name->select('initials', 'surname')->where('id', '=', $user->ref_id)->first();
+                $data['converstations'][$key]['user']['name'] = $participant_name->initials." ".$participant_name->surname;
 
             endforeach;
 
             $this->load->view('messages/index', $data);
         }
 
-
     }
 
+    public function ajax_send_msg(){
+
+        //CHECK LOGIN STATUS
+		if( !isset($_SESSION['user']) OR $_SESSION['user']['is_login'] != true ):
+			header( 'Location:' . $this->config->get('base_url') . '/logout' );
+			exit();
+		endif;
+
+		/**
+		  * This method will receive ajax request from
+		  * the front end with the payload
+		  * 
+		  *	- participant_id, msg
+		  * 
+		  * We need to validate the data and then perform
+		  * the following tasks.
+		  *    - validate
+		  *    - CRUD
+		  *    - response ( JSON )
+		  */
+ 
+        // SET JSON HEADER
+        header('Content-Type: application/json');
+        
+		//  MODEL
+        $this->load->model('user');
+        $this->load->model('message');
+
+		// VALIDATION : participant_id
+		$is_valid_participant_id = GUMP::is_valid($this->request->post, array('participant_id' => 'required|numeric|min_len,1|max_len,8'));
+		if ( $is_valid_participant_id !== true ):
+			echo json_encode( array( "status" => "failed", "message" => "Please select a valid conversation" ), JSON_PRETTY_PRINT );
+			exit();
+        endif;
+
+        // VALIDATE staff_type ID
+		$is_exist = $this->model_user->select('id')->where('id', '=', $this->request->post['participant_id'])->first();
+		if( $is_exist == NULL ):
+			echo json_encode( array( "status" => "failed", "message" => "Message sending failed. user doesn't exists" ), JSON_PRETTY_PRINT );
+			exit();
+		endif;
+        
+        // VALIDATION : message
+		$is_valid_message = GUMP::is_valid($this->request->post, array('message' => 'required'));
+		if ( $is_valid_message !== true ):
+			echo json_encode( array( "status" => "failed", "message" => "Please type a message" ), JSON_PRETTY_PRINT );
+			exit();
+        endif;
+
+        $this->model_message->sender_id = $_SESSION['user']['id'];
+        $this->model_message->receiver_id = $this->request->post['participant_id'];
+        $this->model_message->body = $this->request->post['message'];
+		
+		// SUBMIT
+		if ( $this->model_message->save() ):
+            echo json_encode( array( "status" => "success" ), JSON_PRETTY_PRINT );
+            exit();
+		else:
+            echo json_encode( array( "status" => "failed", "message" => "Unable to send message. Please contact your System Administrator" ), JSON_PRETTY_PRINT );
+            exit();
+		endif;
+    }
 }
 ?>
