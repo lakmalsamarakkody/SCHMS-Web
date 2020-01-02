587d21435714f50e768a5a34c7f4cf247d5a4f19
Srivin Prabhash <me@srivin.pw>
2020-01-02 22:32:10 +0530

    fixed timetable bug

--- app/controllers/Timetable.php
+++ app/controllers/Timetable.php
@@ -195,114 +195,6 @@ class Timetable extends Controller {
         $this->load->view('timetable/index', $data);        
     }
     
-    public function class_timetable() {
-
-        //CHECK LOGIN STATUS
-		if( !isset($_SESSION['user']) OR $_SESSION['user']['is_login'] != true ):
-			header( 'Location:' . $this->config->get('base_url') . '/logout' );
-			exit();
-		endif;
-    
-        // SITE DETAILS
-		$data['app']['url']			= $this->config->get('base_url');
-		$data['app']['title']		= $this->config->get('site_title');
-		$data['app']['theme']		= $this->config->get('app_theme');
-
-		// HEADER / FOOTER
-		$data['template']['header']		= $this->load->controller('common/header', $data);
-        $data['template']['footer']		= $this->load->controller('common/footer', $data);
-        $data['template']['sidenav']	= $this->load->controller('common/sidenav', $data);
-        $data['template']['topmenu']	= $this->load->controller('common/topmenu', $data);
-
-        // MODEL
-        $this->load->model('grade');
-        $this->load->model('subject');
-        $this->load->model('staff');
-        $this->load->model('class');
-        $this->load->model('class/timetable');
-        
-        //STUDENT CLASS
-        foreach( $this->model_class->select('id', 'grade_id', 'staff_id', 'name')->get() as $key => $element ):
-            $data['classes'][$key]['id'] = $element->id;
-            $data['classes'][$key]['grade']['id'] = $element->grade_id;
-            $data['classes'][$key]['staff']['id'] = $element->staff_id;
-            $data['classes'][$key]['name'] = $element->name;
-
-            $data['classes'][$key]['grade']['name'] = $this->model_grade->select('name')->where('id', '=', $element->grade_id)->first()->name;
-        endforeach;
-
-        // SUBJECTS
-        foreach( $this->model_subject->select('id', 'name', 'si_name')->orderBy('name')->get() as $key => $element ):
-            $data['subject'][$key]['id'] = $element->id;
-            $data['subject'][$key]['name'] = $element->name;
-            $data['subject'][$key]['si_name'] = $element->si_name;
-        endforeach;
-
-        // STAFF
-        foreach( $this->model_staff->select('id', 'initials', 'surname')->orderBy('surname')->get() as $key => $element ):
-            $data['staffs'][$key]['id'] = $element->id;
-            $data['staffs'][$key]['initials'] = $element->initials;
-            $data['staffs'][$key]['surname'] = $element->surname;
-            
-        endforeach;
-
-        // CLASS TIME TABLE
-        if ( isset($this->request->post['isSubmitedClassTimeTable']) ):
-            
-            // PRESERVE SUBMITED DATA
-            $data['form']['field']['timetable_class'] = ( isset($this->request->post['timetable_class']) AND !empty($this->request->post['timetable_class']) ) ? $this->request->post['timetable_class'] : "";
-
-            // VALIDATE CLASS
-            // GUMP VALIDATION
-            $gump = new GUMP();
-            $gump->validation_rules(array(
-                'timetable_class'     => 'required|numeric|min_len,1|max_len,3'
-            ));
-
-            if ( $gump->run($this->request->post) === false ):
-                $data['error']['status'] = true;
-                $data['error']['msg'] = "Invalid class selected. Refresh your Browser and try again";
-            else:
-                // DB VALIDATION : CLASS
-                if ( $this->model_class->select('id')->where('id', '=', $this->request->post['timetable_class'])->first() != NULL ):
-
-                    $data['class']['id'] = $this->request->post['timetable_class'];
-
-                    $class_timetable = $this->model_class_timetable->select('id', 'class_id', 'day', 'period', 'subject_id', 'staff_id')->where('class_id', '=', $this->request->post['timetable_class']);
-                    if ( $class_timetable =! NULL ):
-
-                        foreach ( $this->model_class_timetable->select('id', 'class_id', 'day', 'period', 'subject_id', 'staff_id')->where('class_id', '=', $this->request->post['timetable_class'])->get() as $key => $element ):
-
-                            $subject = $this->model_subject->select('name')->where('id', '=', $element->subject_id)->first();
-                            $staff = $this->model_staff->select('initials', 'surname')->where('id', '=', $element->staff_id)->first();
-
-                            $data['timetables'][$element->day] = array(
-                                'period'    => array(
-                                    'id'            => $element->period,
-                                    'subject_name'  => ( $subject !== NULL ) ? $subject->name : null,
-                                    'staff_name'    =>  ( $staff !== NULL ) ? $staff->initials." ".$staff->surname : null,
-                                )
-                            );
-
-                        endforeach;
-
-                    else:
-                        $data['error']['status'] = true;
-                        $data['error']['msg'] = "No schedule to selected class";
-                    endif;
-
-                else:
-                    $data['error']['status'] = true;
-                    $data['error']['msg'] = "No class found. Please try again";
-                endif;
-            endif;
-
-        endif;
-
-		// RENDER VIEW
-        $this->load->view('timetable/class_timetable', $data);
-        
-    }
     
     public function staff_timetable() {
 
@@ -515,6 +407,75 @@ class Timetable extends Controller {
         $this->load->view('timetable/create', $data);
         
     }
+
+
+    public function class_timetable() {
+
+        //CHECK LOGIN STATUS
+        if( !isset($_SESSION['user']) OR $_SESSION['user']['is_login'] != true ):
+            header( 'Location:' . $this->config->get('base_url') . '/logout' );
+            exit();
+        endif;
+    
+        // SITE DETAILS
+        $data['app']['url']         = $this->config->get('base_url');
+        $data['app']['title']       = $this->config->get('site_title');
+        $data['app']['theme']       = $this->config->get('app_theme');
+
+        // HEADER / FOOTER
+        $data['template']['header']     = $this->load->controller('common/header', $data);
+        $data['template']['footer']     = $this->load->controller('common/footer', $data);
+        $data['template']['sidenav']    = $this->load->controller('common/sidenav', $data);
+        $data['template']['topmenu']    = $this->load->controller('common/topmenu', $data);
+
+        // MODEL
+        $this->load->model('class');
+        $this->load->model('class/timetable');
+        $this->load->model('grade');
+        $this->load->model('subject');
+        $this->load->model('staff');
+
+        // GIVE ALL CLASSES
+        //STUDENT CLASS
+        foreach( $this->model_class->select('id', 'grade_id', 'staff_id', 'name')->orderBy('grade_id')->get() as $key => $element ):
+            $data['classes'][$key]['id'] = $element->id;
+            $data['classes'][$key]['grade']['id'] = $element->grade_id;
+            $data['classes'][$key]['staff']['id'] = $element->staff_id;
+            $data['classes'][$key]['name'] = $element->name;
+
+            $data['classes'][$key]['grade']['name'] = $this->model_grade->select('name')->where('id', '=', $element->grade_id)->first()->name;
+        endforeach;
+
+        // SUBS
+        $data['subject'] = $this->model_subject->select('id', 'name', 'si_name')->orderBy('id')->get();
+
+        // STAFF
+        $data['staffs'] = $this->model_staff->select('id', 'initials', 'surname')->orderBy('surname')->get();
+
+        // CHECK IF POST REQUEST
+        if ( !empty($this->request->post) ){
+
+            $data['class']['id'] = $this->request->post['timetable_class'];
+             // PRESERVE SUBMITED DATA
+            $data['form']['field']['timetable_class'] = ( isset($this->request->post['timetable_class']) AND !empty($this->request->post['timetable_class']) ) ? $this->request->post['timetable_class'] : "";
+
+
+            // GET TIMETABLE OF CLASS
+            foreach ( $this->model_class_timetable->select('id', 'class_id', 'day', 'period', 'subject_id', 'staff_id')->where('class_id', '=', $this->request->post['timetable_class'])->get() as $key => $element ):
+                $data['timetable'][$element->day][$element->period] = array(
+                    'subject_id' => $element->subject_id,
+                    'staff_id' => $element->staff_id
+                );
+            endforeach;
+
+        }
+
+        // RENDER VIEW
+        $this->load->view('timetable/class_timetable', $data);
+        
+    }
+
+
 
     // THIS METHOD WILL MANIPULATE SUBJECT PER EACH CLASS
     public function ajax_assign_subject() {
